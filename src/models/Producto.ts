5bca7cf0a75763f3734c22646c740d44a7687b8c
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-08 14:42:50 -0600

    feat: Modelo Producto.

---
+++ src/models/Producto.ts
@@ -0,0 +1,25 @@
+import { Column, Model, Table, CreatedAt, UpdatedAt, ForeignKey, PrimaryKey, AutoIncrement } from 'sequelize-typescript';
+import {Categoria} from "./Categoria";
+
+@Table
+export class Producto extends Model<Producto> {
+
+    @Column
+    @PrimaryKey
+    @AutoIncrement
+    id_producto: number;
+
+    @Column
+    nombre: string;
+
+    @Column
+    descripcion: string;
+
+    @Column
+    precio: number;
+
+    @ForeignKey(() => Categoria)
+    @Column
+    Categoria_id_categoria!: number;
+
+}
