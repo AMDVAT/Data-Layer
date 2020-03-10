10ff7ece207123ed4756fb1df29d86d3d290a4ed
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-09 21:53:51 -0600

    feat: Modelo Tipo Usuario

--- src/models/TipoUsuario.ts
+++ src/models/TipoUsuario.ts
@@ -1,6 +1,6 @@
 import { Column, Model, Table, CreatedAt, UpdatedAt, PrimaryKey, AutoIncrement } from 'sequelize-typescript';
 
-@Table
+@Table({ modelName: 'tipo_usuario'})
 export class TipoUsuario extends Model<TipoUsuario> {
 
     @Column
